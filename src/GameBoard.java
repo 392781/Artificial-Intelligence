17b01f6a1b86a53c81d845d55a2987f1856c19c9
Ronald Lencevicius <rlencevicius@gmail.com>
2018-11-29 19:44:54 -0800

    Removed untracked files (IDE specific)

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,7 @@
 public class GameBoard {
 	private char[][] board = new char[8][8];
+	private byte[][] positions;
+	private int[][] weights;
   
   public GameBoard() {
     for (int i = 0; i < board.length; i++) {
